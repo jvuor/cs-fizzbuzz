2f878ed2b434977a6b080fda823a0a5f8f710e23
jvuor <jussi.vuorinen@gmail.com>
2018-12-29 14:50:35 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static System.Collections.Generic.IEnumerable<int> Range(int from, int to) 
+    {
+        for (int i = from; i <= to; i++) 
+        {
+            yield return i;
+        }
+        yield break;
+    }
+
+    static void Main(string[] args)
+    {
+        foreach (int i in Range(1,100)) 
+        {
+            string response = "";
+
+            if(i % 3 == 0)
+            {
+                response += "Fizz";
+            }
+
+            if(i % 5 == 0)
+            {
+                response += "Buzz";
+            }
+
+            if(response == "")
+            {
+                response = i.ToString();
+            }
+
+            Console.WriteLine(response);
+        }
+    }
+}
